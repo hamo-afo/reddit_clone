961a32fbb8e6f0d88f202db454489f3b3056498a
hamo_afo <hamza472afzal@gmail.com>
2026-02-07 13:51:02 +0500

    Initial commit - Reddit clone app with authentication and community features

---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:reddit_tutorial/features/auth/screens/login_screen.dart';
+import 'package:reddit_tutorial/features/community/screens/community_screen.dart';
+import 'package:reddit_tutorial/features/community/screens/create_community_screen.dart';
+import 'package:reddit_tutorial/features/home/screens/home_screen.dart';
+import 'package:routemaster/routemaster.dart';
+
+final loggedOutRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: LoginScreen()),
+});
+
+final loggedInRoute = RouteMap(routes: {
+  '/': (_) => const MaterialPage(child: HomeScreen()),
+  '/create-community': (_) => const MaterialPage(child: CreateCommunityScreen()),
+  '/r/:name': (route) =>  MaterialPage(
+    child: CommunityScreen(
+    name: route.pathParameters['name']!,
+  )),
+});
+
+
