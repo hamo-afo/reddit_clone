cbb9c799b8d0c53995160f2b895109e82bc87218
hamo_afo <hamza472afzal@gmail.com>
2026-03-07 22:59:24 +0500

    Add Bottom Nav Bar & Add Post Screen UI

---
+++ lib/features/feed/feed_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class FeedScreen extends ConsumerWidget {
+  const FeedScreen({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO: implement build
+    return Container();
+  }
+}
